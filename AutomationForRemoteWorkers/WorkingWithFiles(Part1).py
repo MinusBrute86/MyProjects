7dd3b4b37c8628f4813b57bacc471605abdd3155
Nathaniel E Steckel <nesteck01@aol.com>
2020-09-01 16:43:56 -0400

    Delete WorkingWithFiles(Part1).py

--- AutomationForRemoteWorkers/WorkingWithFiles(Part1).py
+++
@@ -1,8 +0,0 @@
-import os  # Gives access to .listdir method
-
-data_location ='C:/Users/natste7487/Documents/'   # Path we want to go through
-file_list = []  # Declares/Makes an empty list
-for file in os.listdir(data_location):  # For each file in path
-    file_list.append(file)  # Aggregates each file in path into empty list
-
-print(file_list)
